7466f705c10eef2a332d869305707db694e8e414
Diego Ramos Ruggeri <diego@ruggeri.net.br>
2014-04-29 02:04:48 -0300

    Comments in portuguese for the hello world module

--- 00_hello/hello.c
+++ 00_hello/hello.c
@@ -2,20 +2,26 @@
 #include <linux/init.h>	
 #include <linux/module.h>
 
+/* Função que será chamada quando o modulo é instalado */
+
 static int __init hello_module(void)
 {
 	printk(KERN_INFO "Hello World!\n");
 	return 0;
 }
 
+/* Função que será chamada quando o modulo é removido  */
+
 static void __exit goodbye_module(void)
 {
 	printk(KERN_INFO "adios\n");
 }
 
-
+/* Registra quais funções devem ser chamadas para cada "evento"  */
 module_init(hello_module);
 module_exit(goodbye_module);
+
+/* Informações sobre o módulo  */
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Diego Ruggeri");
