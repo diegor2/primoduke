23c02033e5b6612811d343fe8c124394124485e5
Diego Ramos Ruggeri <diego@ruggeri.net.br>
2014-05-02 01:02:54 -0300

    Test scripts for hello world module

---
+++ 00_hello/tests.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+clear
+
+# testa se o usuário é root
+# check if user is root
+if [ $(id -un) != "root" ]; then
+   echo -e "Esse script deve executar com usuário root" 1>&2
+   echo -e "This script must be run as root user\n" 1>&2
+   echo -e "e.g: sudo bash test.sh\n\n"
+   exit 1
+fi
+
+#install the module
+echo -e "instala o modulo\n"
+echo -e "# insmod hello.ko\n"
+insmod hello.ko
+read -p "Continuar ... "
+echo -e "----------------------------------------------"
+
+#list installed modules
+echo -e "lista os modulos instalados\n"
+echo -e "$ lsmod | head \n"
+lsmod | head
+echo -e "\n"
+read -p "Continuar ... "
+echo -e "----------------------------------------------"
+
+# message on dmesg
+echo -e "mensagem no dmesg (display messages)\n"
+echo -e "$ dmesg | tail\n"
+dmesg | tail
+echo -e "\n"
+read -p "Continuar ... "
+echo -e "----------------------------------------------"
+
+#remove the module 
+echo -e "remove o module\n"
+echo -e "# rmmod hello.ko\n"
+rmmod hello.ko
+read -p "Continuar ... "
+echo -e "----------------------------------------------"
+
+
+# message on dmesg
+echo -e "mensagem no dmesg (display messages)\n"
+echo -e "dmesg | tail\n"
+dmesg | tail
+echo -e "----------------------------------------------"
